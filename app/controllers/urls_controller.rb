68c3cf646cc4bce6967717daf0b45b7fbae328f2
Sergey <sukyans@gmail.com>
2020-05-23 23:51:20 -0600

    initial

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,32 +3,51 @@
 class UrlsController < ApplicationController
   def index
     @url = Url.new
-    @urls = [
-      Url.new(short_url: '123', original_url: 'http://google.com', created_at: Time.now),
-      Url.new(short_url: '456', original_url: 'http://facebook.com', created_at: Time.now),
-      Url.new(short_url: '789', original_url: 'http://yahoo.com', created_at: Time.now)
-    ]
+    # @urls = [
+    #   Url.new(short_url: '123', original_url: 'http://google.com', created_at: Time.now),
+    #   Url.new(short_url: '456', original_url: 'http://facebook.com', created_at: Time.now),
+    #   Url.new(short_url: '789', original_url: 'http://yahoo.com', created_at: Time.now),
+    # ]
+    @urls = Url.all
   end
 
   def create
-    # create a new URL record
+    original_url = params[:url][:original_url]
+    @url = Url.new()
+    @url.short_url = @url.generate_short_url
+    if @url.validate_url(original_url)
+      @url.original_url = original_url
+    else
+      redirect_to root_path, notice: 'URL is incorrect' and return
+    end
+    @url.created_at = Time.now
+    @url.save
+    redirect_to root_path
   end
 
   def show
-    @url = Url.new(short_url: '123', original_url: 'http://google.com', created_at: Time.now)
-    # implement queries
-    @daily_clicks = [
-      ['1', 13],
-      ['2', 2],
-      ['3', 1],
-      ['4', 7],
-      ['5', 20],
-      ['6', 18],
-      ['7', 10],
-      ['8', 20],
-      ['9', 15],
-      ['10', 5]
-    ]
+    @url = Url.find_by(short_url: params[:url])
+
+    beginning_of_the_month = Date.today.at_beginning_of_month
+    today_date = Date.today
+    days = [*(beginning_of_the_month..today_date)]
+
+    @daily_clicks = []
+    days.each do |day|
+      @daily_clicks << [day.to_s.split("-").last.to_i, Url.where("created_at LIKE ?", day)]
+    end
+    # @daily_clicks = [
+    #   ['1', 13],
+    #   ['2', 2],
+    #   ['3', 1],
+    #   ['4', 7],
+    #   ['5', 20],
+    #   ['6', 18],
+    #   ['7', 10],
+    #   ['8', 20],
+    #   ['9', 15],
+    #   ['10', 5]
+    # ]
     @browsers_clicks = [
       ['IE', 13],
       ['Firefox', 22],
@@ -44,7 +63,14 @@ class UrlsController < ApplicationController
   end
 
   def visit
-    # params[:url]
-    # @url = find url
+    url = params[:url]
+    @url = Url.where(short_url: url).first.original_url
+    @click = Click.new
+    @click.created_at = Date.today
+    @click.url_id = @url.id
+    @click.browser = "chrome"
+    @click.platform = "macOS"
+    @click.save
+    redirect_to @url
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
-
+require 'open-uri'
 class Url < ApplicationRecord
+  validates :short_url, :presence => true
+  validates :original_url, :presence => true
+
+  has_many :clicks
   # scope :latest, -> {}
+
+  def generate_short_url
+    unique = false
+    while unique != true
+      url = ([*('a'..'z'),*('A'..'Z'), *('0'..'9')]).sample(5).join()
+      short_urls = Url.pluck(:short_url)
+      unique = true if !short_urls.include?(url)
+    end
+    return url
+  end
+
+  def validate_url(original_url)
+    URI.parse(original_url).respond_to?(:open)
+  end
 end
+
+#
+# Max length 5 character e.g. NELNT
+# Allows Uppercase and Lowercase characters
+# Allows Number
+# Any non-word character is not allowed e.g whitespaces, tab,% ,$.* etc
+# URL must be unique
+# Original URL format should be validated
+# shot_url attribute should be stored only the generated code
